6016f773c76ac9a4f69774ed2cb4aaedab41b655
charlesmilk <carlosmagalhaesleite@gmail.com>
2020-03-12 01:55:49 +0000

    Added files

---
+++ get_data.py
@@ -0,0 +1,17 @@
+import urllib.request
+import os
+
+working_dir = os.getcwd()
+output_dir = working_dir + '/data/'
+
+os.mkdir(working_dir + '/data')
+
+file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+
+confirmations = 'time_series_19-covid-Confirmed.csv'
+deaths = 'time_series_19-covid-Deaths.csv'
+recover = 'time_series_19-covid-Recovered.csv'
+
+urllib.request.urlretrieve(file_url + confirmations, output_dir + confirmations)
+urllib.request.urlretrieve(file_url + confirmations, output_dir + deaths)
+urllib.request.urlretrieve(file_url + confirmations, output_dir + recover)
