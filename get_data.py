66b96446503530a5aab3af2acd4257429e27a9a2
charlesmilk <carlosmagalhaesleite@gmail.com>
2020-03-13 13:47:04 +0000

    modified get_data

--- get_data.py
+++ get_data.py
@@ -1,18 +1,23 @@
 import urllib.request
 import os
 
-working_dir = os.getcwd()
+def get_data():
+  working_dir = os.getcwd()
 
-output_dir = working_dir + '/data/'
-if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+  output_dir = working_dir + '/data/'
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+  file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
 
-confirmations = 'time_series_19-covid-Confirmed.csv'
-deaths = 'time_series_19-covid-Deaths.csv'
-recover = 'time_series_19-covid-Recovered.csv'
+  confirmations = 'time_series_19-covid-Confirmed.csv'
+  deaths = 'time_series_19-covid-Deaths.csv'
+  recover = 'time_series_19-covid-Recovered.csv'
 
-urllib.request.urlretrieve(file_url + confirmations, output_dir + confirmations)
-urllib.request.urlretrieve(file_url + deaths, output_dir + deaths)
-urllib.request.urlretrieve(file_url + recover, output_dir + recover)
+  urllib.request.urlretrieve(file_url + confirmations, output_dir + confirmations)
+  urllib.request.urlretrieve(file_url + deaths, output_dir + deaths)
+  urllib.request.urlretrieve(file_url + recover, output_dir + recover)
+
+if __name__ == '__main__':
+  get_data()
+
